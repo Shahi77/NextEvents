159f4ddb8c3c14231de5b72dcce63fc7b3e6b529
Shahi77 <shahiinfinity02@gmail.com>
2023-05-20 23:49:20 +0530

    extracting data on catch all page

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,9 +2,17 @@ import EventList from "@/components/events/event-list";
 import EventsSearch from "@/components/events/events-search";
 import { getAllEvents } from "@/dummy-data";
 import { Fragment } from "react";
+import { useRouter } from "next/router";
 
 function AllEventsPage() {
     const events = getAllEvents();
+    const router = useRouter();
+
+    const findEventsHandler = (year, month) => {
+        const fullPath = `/events/${year}/${month}`;
+        router.push(fullPath);
+    };
+
     return (
         <Fragment>
             <EventsSearch />
